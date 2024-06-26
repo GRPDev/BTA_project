27a3907951308510a10a18c05d1178402cd016cc
GRPDev <gm.romero.papp@gmail.com>
2024-06-26 18:24:52 +0100

    adding filemanager.py

--- FileManager.py
+++ FileManager.py
@@ -2,29 +2,33 @@ import json
 
 class FileManager:
     def load_data(self, filename):
-        pass
-        # TODO:
-        # Implement a process that reads the contents of the `filename` file 
-        # and returns the text.
+        with open(filename, "r") as file:
+            loaddata = file.read()
+            return loaddata
 
     def save_data(self, filename, data):
-        pass
-        # TODO:
-        # Implement a process that writes the contents of `data` to the file `filename`
-
+        with open(filename, "w") as file:
+            file.write(data)
+        
     def read_json(self, json_file_path):
-        pass
+        with open(json_file_path, "r") as file:
+            readjson = json.load(file)
+            print(readjson)
+            
         # TODO:
         # Implement a process that reads the contents of a file whose path is stored in the `json_file_path` variable 
         # and returns a list of dictionaries
         
     def write_json(self, list_of_dicts, json_file_path):
-        pass
+        with open(json_file_path, "w") as file:
+            json.dump(list_of_dicts, file)
         # TODO:
         # Implement a process that writes a list of dictionaries from list_of_dicts to the `json_file_path` file
 
     def add_to_json(self, data, json_file_path):
-        pass
+        with open(json_file_path, "a") as file:
+            json.dump(data, file)
+        
         # TODO:
         # Implement a process that gets the dictionary in the data variable and adds it to the JSON `json_file_path`
 
