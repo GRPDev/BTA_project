d5ca83ca235d7a09ac259fd5e9f77240ab334bdb
GRPDev <gm.romero.papp@gmail.com>
2024-06-27 21:08:57 +0100

    reviewed filemanager

--- FileManager.py
+++ FileManager.py
@@ -13,23 +13,14 @@ class FileManager:
     def read_json(self, json_file_path):
         with open(json_file_path, "r") as file:
             readjson = json.load(file)
-            print(readjson)
-            
-        # TODO:
-        # Implement a process that reads the contents of a file whose path is stored in the `json_file_path` variable 
-        # and returns a list of dictionaries
+            return readjson
         
     def write_json(self, list_of_dicts, json_file_path):
         with open(json_file_path, "w") as file:
             json.dump(list_of_dicts, file)
-        # TODO:
-        # Implement a process that writes a list of dictionaries from list_of_dicts to the `json_file_path` file
-
+       
     def add_to_json(self, data, json_file_path):
-        with open(json_file_path, "a") as file:
-            json.dump(data, file)
-        
-        # TODO:
-        # Implement a process that gets the dictionary in the data variable and adds it to the JSON `json_file_path`
-
+        data_in_json = self.read_json(json_file_path)
+        data_in_json.append(data)
+        self.write_json(data_in_json, json_file_path)  
             
