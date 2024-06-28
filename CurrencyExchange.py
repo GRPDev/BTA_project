50d487ed5797ea5bf3ebbc085d8fa3679bc25b54
GRPDev <gm.romero.papp@gmail.com>
2024-06-28 18:32:49 +0100

    passed tests currency exch

--- CurrencyExchange.py
+++ CurrencyExchange.py
@@ -1,7 +1,6 @@
 from FileManager import FileManager
 from HistoryMessages import HistoryMessages
-import requests
-import json
+import request
 
 class CurrencyExchange:
     def __init__(self, balance = 0):
@@ -28,10 +27,10 @@ class CurrencyExchange:
     def exchange_currency(self, currency_from, currency_to, amount):
         # implement a process that transfers the specified amount from currency `currency_from` 
         # to currency `currency_to` and, if positive, returns the amount in the new currency
+        
         rates = self.get_exchange_rates() #json.loads(exc_rates.text)
         try:
             amount = float(amount)
-        
         except Exception:
             print('Currency exchange failed!')
             history_message = HistoryMessages.exchange("failure", amount, None, currency_from, currency_to)
@@ -41,25 +40,25 @@ class CurrencyExchange:
         if amount <= 0:
             history_message = HistoryMessages.exchange("failure", amount, None, currency_from, currency_to)
             self.write_to_history(history_message)
-            raise ValueError("Currency exchange failed!")
+            print("Currency exchange failed!")
             
         elif currency_from not in rates or currency_to not in rates:
             history_message = HistoryMessages.exchange("failure", amount, None, currency_from, currency_to)
             self.write_to_history(history_message)
-            raise ValueError("Currency exchange failed!")
+            print("Currency exchange failed!")
 
         elif currency_from == "EUR":    #exchange EUR to any currency
             converted_amount = rates[currency_to] * amount
             history_message = HistoryMessages.exchange("success", amount, converted_amount, currency_from, currency_to)
             self.write_to_history(history_message)
             return converted_amount
-                     
+                        
         elif currency_from != "EUR" and currency_to == "EUR":   #exchange any currency to EUR
             converted_amount = amount / rates[currency_from]
             history_message = HistoryMessages.exchange("success", amount, converted_amount, currency_from, currency_to)
             self.write_to_history(history_message)
             return converted_amount
-            
+                
         elif currency_from != "EUR" and currency_to != "EUR":   #exchange any currency to any currency
             temp_conversion = amount / rates[currency_from]     #convert any currency to EUR
             converted_amount = rates[currency_to] * temp_conversion   #convert EUR to any currency
